f11cf8c1690affe62f5cc1c14bacdb102dee52ef
Minhajuddink7 <Minhajuddink7@gmail.com>
2020-01-04 10:08:10 +0530

    ui changes

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save Bookmark
-function saveBookmark(e){
+function saveBookmark(e) {
   // Get form values
-  var siteName =document.getElementById('siteName').value;
-  var siteUrl =document.getElementById('siteUrl').value;
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
 
-  if(!validateForm(siteName, siteUrl)){
+  if (!validateForm(siteName, siteUrl)) {
     return false;
   }
 
   var bookmark = {
     name: siteName,
     url: siteUrl
-  }
+  };
 
   /*
     // Local Storage Test
@@ -25,7 +25,7 @@ function saveBookmark(e){
   */
 
   // Test if bookmarks is null
-  if(localStorage.getItem('bookmarks') === null){
+  if (localStorage.getItem('bookmarks') === null) {
     // Init array
     var bookmarks = [];
     // Add to array
@@ -52,12 +52,12 @@ function saveBookmark(e){
 }
 
 // Delete bookmark
-function deleteBookmark(url){
+function deleteBookmark(url) {
   // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
   // Loop through the bookmarks
-  for(var i =0;i < bookmarks.length;i++){
-    if(bookmarks[i].url == url){
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
       // Remove from array
       bookmarks.splice(i, 1);
     }
@@ -70,7 +70,7 @@ function deleteBookmark(url){
 }
 
 // Fetch bookmarks
-function fetchBookmarks(){
+function fetchBookmarks() {
   // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
   // Get output id
@@ -78,22 +78,28 @@ function fetchBookmarks(){
 
   // Build output
   bookmarksResults.innerHTML = '';
-  for(var i = 0; i < bookmarks.length; i++){
+  for (var i = 0; i < bookmarks.length; i++) {
     var name = bookmarks[i].name;
     var url = bookmarks[i].url;
 
-    bookmarksResults.innerHTML += '<div class="well">'+
-                                  '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
-                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
-                                  '</h3>'+
-                                  '</div>';
+    bookmarksResults.innerHTML +=
+      '<div class="well">' +
+      '<h3>' +
+      name +
+      ' <a class="btn btn-default" target="_blank" href="' +
+      addhttp(url) +
+      '">Visit</a> ' +
+      ' <a onclick="deleteBookmark(\'' +
+      url +
+      '\')" class="btn btn-danger" href="#">Delete</a> ' +
+      '</h3>' +
+      '</div>';
   }
 }
 
 // Validate Form
-function validateForm(siteName, siteUrl){
-  if(!siteName || !siteUrl){
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
     alert('Please fill in the form');
     return false;
   }
@@ -101,7 +107,7 @@ function validateForm(siteName, siteUrl){
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   var regex = new RegExp(expression);
 
-  if(!siteUrl.match(regex)){
+  if (!siteUrl.match(regex)) {
     alert('Please use a valid URL');
     return false;
   }
@@ -111,7 +117,7 @@ function validateForm(siteName, siteUrl){
 
 function addhttp(url) {
   if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-      url = "http://" + url;
+    url = 'http://' + url;
   }
   return url;
 }
